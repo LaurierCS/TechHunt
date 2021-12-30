e7ee87dff02bf06ffcd17f260bb4091fe403a564
dhav15 <pate7400@mylaurier.ca>
2021-12-30 17:14:02 -0500

    Removed commented out homepage_view

--- Wampus/app/views.py
+++ Wampus/app/views.py
@@ -1,23 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-"""def homepage_view(request):
-    object1 =  ""Some object queried from the Database, 
-                maybe a project or set of project objects""
-    
-    context = {
-        #You place objects in here that you want to bring 
-        # to the front end. You do it just by adding them
-        # to this dictionary commonly called context -
-        # simply store key and value pairs
-        # For example:
-        'object1':object1
-    }
-
-    template_name = 'dashboard.html'
-
-    return render(request, template_name, context)"""
-
-
 def login_view(request):
     context = {}
     template_name = 'login.html'
