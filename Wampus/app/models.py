c0d145011171a9869c837a361992f2d1634a66b9
aidantrabs <aidantraboulay13@gmail.com>
2022-01-01 19:35:16 -0400

    Updated Profile and Project Model, Created Tag Model

--- Wampus/app/models.py
+++ Wampus/app/models.py
@@ -6,37 +6,12 @@ from django.db.models.fields import CharField
 # Profile Model
 class Profile(models.Model):
 
-    """
-    Description
-    --------------------------------------------------
-    Represents a Profile within the Models.
-    A Profile is a user of the Wampus application.
-    
-    Model Fields
-    --------------------------------------------------
-    user: OneToOneField
-    first_name : str
-    last_name : str
-    bio : str
-    location : str
-    date_created: DateTimeField
-
-    Model Attributes
-    --------------------------------------------------
-    __str__():
-        Returns the string representation of Profile name.
-
-    Author
-    --------------------------------------------------
-    Aidan Traboulay
-    """
-
     # Profile Fields
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50, null=True)
-    last_name = models.CharField(max_length=50, null=True)
-    bio = models.TextField(max_length=500, null=True)
-    location = models.CharField(max_length=50, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    bio = models.TextField(max_length=500, null=True, blank=True)
+    location = models.CharField(max_length=50, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
     # Profile Attributes
@@ -46,38 +21,34 @@ class Profile(models.Model):
 # Project Model 
 class Project(models.Model):
 
-    """
-    Description
-    --------------------------------------------------
-    Represents a Project within the Models.
-    A Project is a project that a user has created.
-
-    Model Fields
-    --------------------------------------------------
-    name : str
-    description : str
-    rating : int
-    date_created: DateTimeField
-    profile: ForeignKey
-
-    Model Attributes
-    --------------------------------------------------
-    __str__():
-        Returns the string representation of Project name.
-    
-    Author
-    --------------------------------------------------
-    Aidan Traboulay
-    """
-
     # Project Fields
     name = models.CharField(max_length=100, null=True)
     description = models.CharField(max_length=250, null=True)
     rating = models.IntegerField(default=0, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     # Project Attributes
     def __str__(self):
         return self.name
+
+# Tag Model
+class Tag(models.Model):
+    
+        # Choices for Tag Type
+        TYPE_CHOICES = (
+            ('T', 'Technology'),
+            ('M', 'Miscellaneous'),
+        )
+
+        # Tag Fields
+        name = models.CharField(max_length=50, null=True)
+        date_created = models.DateTimeField(auto_now_add=True)
+        type_selection = models.CharField(max_length=1, choices=TYPE_CHOICES)
+        projects = models.ManyToManyField('Project', blank=True)
+    
+        # Tag Attributes
+        def __str__(self):
+            return self.name
 
