feb3042874e0a50ddf6bd83d2397ec4899724067
aidantrabs <aidantraboulay13@gmail.com>
2022-01-01 19:35:38 -0400

    registered tag model

--- Wampus/app/admin.py
+++ Wampus/app/admin.py
@@ -1,9 +1,12 @@
 # Imports
 from django.contrib import admin
-from .models import Profile, Project
+from .models import Profile, Project, Tag
 
 # Register Profile Model
 admin.site.register(Profile)
 
 # Register Project Model
 admin.site.register(Project)
+
+# Register Tag Model
+admin.site.register(Tag)
