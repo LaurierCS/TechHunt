a02fa27c99c6c7b39aa675d405a63a000d39e03c
aidantrabs <aidantraboulay13@gmail.com>
2022-01-01 19:36:39 -0400

    re-added admin panel url route

--- Wampus/Wampus/urls.py
+++ Wampus/Wampus/urls.py
@@ -18,9 +18,10 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls), # Do Not Remove
     path('homepage/', views.homepage_view),
     path('login/', views.login_view),
     path('register/', views.register_view),
     path('project/', views.project_view),
-    path('profile/', views.profilePage_view),
+    path('profile/', views.profilepage_view),
 ]
