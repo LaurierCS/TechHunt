680d36bc0c51b17f0574d78f35b96b9dffe36590
aidantrabs <aidantraboulay13@gmail.com>
2022-01-02 16:13:14 -0400

    added admin model & properties for dev purposes

--- Wampus/app/admin.py
+++ Wampus/app/admin.py
@@ -2,11 +2,19 @@
 from django.contrib import admin
 from .models import Profile, Project, Tag, Favorite, Comment
 
+# Profile Model Admin 
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
 # Register Profile Model
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
+
+# Project Model Admin
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
 
 # Register Project Model
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 
 # Register Favorite Model
 admin.site.register(Favorite)
