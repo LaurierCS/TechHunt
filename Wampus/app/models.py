736f5f61246784bff240ecdc6b1f21d4921f8d7f
Isaac <adda9960@mylaurier.ca>
2022-01-03 19:19:32 -0500

    Create Category Model

--- Wampus/app/models.py
+++ Wampus/app/models.py
@@ -31,6 +31,7 @@ class Project(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE, null=True)
     tags = models.ManyToManyField('Tag', blank=True)
+    category = models.ForeignKey("Category", blank = True, null = True, on_delete=models.SET_NULL)
 
     # Project Attributes
     def __str__(self):
@@ -74,3 +75,15 @@ class Comment(models.Model):
     # Comment Attributes
     def __str__(self):
         return self.text
+
+    # Category Model
+class Category(models.Model):
+
+    # Category Fields
+
+    project = models.Charfield(max_length=50, null=True)
+
+    # Category Attributes
+    def __str__(self):
+        return self.name
+        
