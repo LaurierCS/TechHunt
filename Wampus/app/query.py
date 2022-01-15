fe329a20f0a4e447ab01b6f75668cc41b9d0451f
aidantrabs <aidantraboulay13@gmail.com>
2022-01-14 22:38:28 -0400

    created query utility function

---
+++ Wampus/app/query.py
@@ -0,0 +1,18 @@
+def search_projects(request):
+
+    query = request.GET.get('query') # Get the search query
+
+    # If the query is empty, return all projects
+    if query == '':
+        return Project.objects.all()
+
+    search_results = Project.objects.filter(
+        Q(name__icontains=query) |
+        Q(description__icontains=query) |
+        Q(profile__user__username__icontains=query) |
+        Q(tags__name__icontains=query) |
+        Q(categories__name__icontains=query)
+    )
+
+    # Otherwise, return the queried projects by name, description, tags, categories or users
+    return search_results
