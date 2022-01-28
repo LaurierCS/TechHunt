41ea753e609adfa0ecad0794b3738484d3ac32d5
aidantrabs <aidantraboulay13@gmail.com>
2022-01-28 02:49:42 -0500

    form import :P

--- Wampus/app/forms.py
+++ Wampus/app/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Project
+from .models import Project, Comment
 
 # Form for regsitering a new user
 class RegisterForm(UserCreationForm):
