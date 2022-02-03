26885197450b8678f220387b35d27553747d9ef0
dhav15 <pate7400@mylaurier.ca>
2022-02-03 16:11:33 -0500

    Created landing page and updated registration and about us pages

--- Wampus/app/views.py
+++ Wampus/app/views.py
@@ -230,3 +230,20 @@ def editprofile_view(request):
     template_name = 'edit-profile.html'
 
     return render(request, template_name, context)
+
+def landing_view(request):
+    object1 = """Some object queried from the Database, 
+                maybe a project or set of project objects"""
+
+    context = {
+        # You place objects in here that you want to bring
+        # to the front end. You do it just by adding them
+        # to this dictionary commonly called context -
+        # simply store key and value pairs
+        # For example:
+        'object1': object1
+    }
+
+    template_name = 'landing.html'
+
+    return render(request, template_name, context)
