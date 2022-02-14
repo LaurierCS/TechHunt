bf6aae6db8e2a02c55e2289969040122507b1ff4
ObiTracks <straightup.stack@gmail.com>
2022-02-14 01:00:13 -0500

    Fix made to settings.py

--- Wampus/settings.py
+++ Wampus/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'app.apps.AppConfig',  # Added main app
+    'app',  # Added main app
 ]
 
 MIDDLEWARE = [
