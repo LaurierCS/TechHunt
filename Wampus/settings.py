e4cd7967e15ae0f40353ef048bcd43f1d31fee5e
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-02-17 04:23:15 -0500

    updated settings

--- Wampus/settings.py
+++ Wampus/settings.py
@@ -14,19 +14,20 @@ import os
 import environ
 import django_heroku
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-socapu5gn7i#(=9k1#aby#&65j@z0)e@1#)8=1@6-2^7gp!(l4'
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'techunt.herokuapp.com', ]
