24b4c1401b84dc77d84022655c25751b994162af
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-02-22 18:14:49 -0500

    updated settings

--- Wampus/settings.py
+++ Wampus/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'app.apps.AppConfig'  # Added main app
+    'app.apps.AppConfig',  # Added main app
 ]
 
 MIDDLEWARE = [
