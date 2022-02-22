0534ebc846f4d80af03fb895f54da7a7d0f88762
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-02-22 18:10:55 -0500

    updated settings

--- Wampus/settings.py
+++ Wampus/settings.py
@@ -27,7 +27,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'techunt.herokuapp.com', ]
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'app',  # Added main app
+    'app.apps.AppConfig'  # Added main app
 ]
 
 MIDDLEWARE = [
