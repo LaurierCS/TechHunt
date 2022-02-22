ac0c2dd3c0636239ab030de8642d071c1cf3be31
dhav15 <pate7400@mylaurier.ca>
2022-02-22 17:27:01 -0500

    Added whitenoise line

--- Wampus/settings.py
+++ Wampus/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'corsheaders.middleware.CorsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Wampus.urls'
