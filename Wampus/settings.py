8763a84832b7b271d2be7cb2155cc0378a37d54b
dhav15 <pate7400@mylaurier.ca>
2022-02-22 18:46:10 -0500

    trying to solve server error

--- Wampus/settings.py
+++ Wampus/settings.py
@@ -27,7 +27,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'techunt.herokuapp.com', ]
